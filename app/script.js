9d94468af2da9490a9d5bdd9ebb31c244ba93bf0
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-04 18:50:45 -0300

    feat: owl carousel js settings

--- app/script.js
+++ app/script.js
@@ -13,17 +13,41 @@ $(document).ready(function() {
             $('.navbar').removeClass('cng-navbar');
         }
     })
+
+    // sample video popup
+    $(document).ready(function() {
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+    });
+
+    // team carousel
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            }, 
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
 })
 
-// sample video popup
-$(document).ready(function() {
-    $('.popup-youtube').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
 
-        fixedContentPos: false
-    });
-});
+
