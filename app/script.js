3fbeb581711a1bf800ccf53beb20ef0d8b85aed5
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-04 14:11:59 -0300

    feat: magnific popup script

--- app/script.js
+++ app/script.js
@@ -14,3 +14,16 @@ $(document).ready(function() {
         }
     })
 })
+
+// sample video popup
+$(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
