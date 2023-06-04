b5f62d0167ebe6b3124363b7c8105c592e11f243
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-04 20:53:54 -0300

    feat: faq accordion js

--- app/script.js
+++ app/script.js
@@ -47,6 +47,20 @@ $(document).ready(function() {
             }
         }
     });
+
+    // faq accordion
+    $('.faq-head').each(function(){
+        $(this).click(function(){
+            $(this).next().toggleClass('show-faq-content');
+            let icon = $(this).children('span').children('i').attr('class');
+
+            if(icon == 'fas fa-plus'){
+                $(this).children('span').html('<i class="fas fa-minus"></i>');
+            } else {
+                $(this).children('span').html('<i class="fas fa-plus"></i>');
+            }
+        });
+    });
 })
 
 
