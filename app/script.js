4fe945c89cea81d06130a1f1b35ea1e4929faab3
Luana Alves <luanaalvessoares_@outlook.com>
2023-06-05 08:31:48 -0300

    feat: testimonials carousel

--- app/script.js
+++ app/script.js
@@ -61,6 +61,15 @@ $(document).ready(function() {
             }
         });
     });
+
+    // testimonial carousel
+    $('.testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        items: 1
+    })
 })
 
 
